19df9ea1dbbe5dd70d3b8f4510a041c6e7b1bda6
Ithyx <lamidey.m@gmail.com>
2025-06-09 21:47:11 -0400

    Switch to rocket managed state and to registry to hold this state

--- src/command.rs
+++ src/command.rs
@@ -1,10 +1,41 @@
-use anyhow::Result;
-use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    process::{Child, ExitStatus},
+};
 
-#[derive(Debug, Clone, Serialize)]
+use anyhow::Result;
+use serde::{Deserialize, Serialize, ser::SerializeStructVariant};
+
+use crate::configuration::Config;
+
+#[derive(Debug, Clone)]
 pub enum Status {
+    NotStarted,
     Running,
-    Exited(String),
+    Exited(ExitStatus),
+}
+
+impl Serialize for Status {
+    fn serialize<S>(&self, s: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match self {
+            Status::NotStarted => {
+                let s = s.serialize_struct_variant("Status", 0, "NotStarted", 0)?;
+                s.end()
+            }
+            Status::Running => {
+                let s = s.serialize_struct_variant("Status", 1, "Running", 0)?;
+                s.end()
+            }
+            Status::Exited(exit_status) => {
+                let mut s = s.serialize_struct_variant("Status", 2, "Exited", 1)?;
+                s.serialize_field("exit status", &exit_status.to_string())?;
+                s.end()
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -21,5 +52,67 @@ impl Command {
             .spawn()?;
 
         Ok(child)
+    }
+}
+
+#[derive(Debug)]
+pub struct Registry {
+    statuses: HashMap<String, Status>,
+    process_handles: HashMap<String, Option<Child>>,
+}
+
+impl Registry {
+    pub fn new(config: &Config) -> Self {
+        let statuses = config
+            .commands
+            .keys()
+            .map(|name| (name.clone(), Status::NotStarted))
+            .collect();
+        let process_handles = config
+            .commands
+            .keys()
+            .map(|name| (name.clone(), None))
+            .collect();
+
+        Self {
+            statuses,
+            process_handles,
+        }
+    }
+
+    pub fn get_latest_status(&mut self, command_name: String) -> Result<Status, String> {
+        let last_status = self
+            .statuses
+            .get_mut(&command_name)
+            .ok_or("no such command")?;
+
+        match last_status {
+            Status::Running => {
+                let child = self
+                    .process_handles
+                    .get_mut(&command_name)
+                    .ok_or("no such running command")?
+                    .as_mut()
+                    .ok_or("command not running")?;
+
+                match child.try_wait().map_err(|err| err.to_string())? {
+                    Some(exit_status) => {
+                        let new_status = Status::Exited(exit_status);
+                        *last_status = new_status.clone();
+
+                        self.process_handles.remove(&command_name);
+
+                        Ok(new_status)
+                    }
+                    None => Ok(last_status.clone()),
+                }
+            }
+            _ => Ok(last_status.clone()),
+        }
+    }
+
+    pub fn register_new_process(&mut self, command_name: String, process: Child) {
+        self.statuses.insert(command_name.clone(), Status::Running);
+        self.process_handles.insert(command_name, Some(process));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 mod command;
 mod configuration;
 
-use std::{
-    collections::HashMap,
-    path::PathBuf,
-    process::Child,
-    sync::{LazyLock, RwLock},
-};
+use std::{path::PathBuf, sync::RwLock};
 
 use clap::{Parser, command};
+use command::Registry;
 use configuration::Config;
 use rocket::serde::json::Json;
 
@@ -19,62 +15,59 @@ struct Args {
     pub configuration: Option<PathBuf>,
 }
 
-static CONFIG: LazyLock<RwLock<Config>> = LazyLock::new(|| RwLock::new(Default::default()));
-static RUNNING_JOBS: LazyLock<RwLock<HashMap<u32, Child>>> =
-    LazyLock::new(|| RwLock::new(Default::default()));
-
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn list_commands() -> Json<Vec<String>> {
-    let commands = CONFIG
-        .read()
-        .unwrap()
-        .commands
-        .clone()
-        .into_keys()
-        .collect();
+fn list_commands(config: &rocket::State<Config>) -> Json<Vec<String>> {
+    let commands = config.commands.keys().cloned().collect();
 
     Json(commands)
 }
 
-fn get_status_inner(command_id: u32) -> Result<command::Status, String> {
-    let mut jobs = RUNNING_JOBS.write().unwrap();
-    let handle = jobs
-        .get_mut(&command_id)
-        .ok_or("no such command".to_owned())?;
+fn get_status_inner(
+    command_name: &str,
+    cmd_reg: &rocket::State<RwLock<command::Registry>>,
+) -> Result<command::Status, String> {
+    let name = command_name.to_owned();
+    let mut registry = cmd_reg.write().unwrap();
 
-    let status = match handle.try_wait().map_err(|err| err.to_string())? {
-        Some(exit_status) => command::Status::Exited(exit_status.to_string()),
-        None => command::Status::Running,
-    };
-
-    Ok(status)
+    registry.get_latest_status(name)
 }
 
-#[get("/status/<command_id>")]
-fn get_status(command_id: u32) -> Json<Result<command::Status, String>> {
-    Json(get_status_inner(command_id))
+#[get("/status/<command_name>")]
+fn get_status(
+    command_name: &str,
+    cmd_reg: &rocket::State<RwLock<command::Registry>>,
+) -> Json<Result<command::Status, String>> {
+    Json(get_status_inner(command_name, cmd_reg))
 }
 
-fn execute_inner(command_name: &str) -> Result<u32, String> {
-    let cfg = CONFIG.read().unwrap();
-    let command = cfg
+fn execute_inner(
+    command_name: &str,
+    config: &rocket::State<Config>,
+    cmd_reg: &rocket::State<RwLock<command::Registry>>,
+) -> Result<(), String> {
+    let command = config
         .commands
         .get(command_name)
         .ok_or("no such command".to_owned())?;
 
-    let child = command.execute().map_err(|err| err.to_string())?;
-    let id = child.id();
-    RUNNING_JOBS.write().unwrap().insert(id, child);
+    let process = command.execute().map_err(|err| err.to_string())?;
 
-    Ok(id)
+    let mut registry = cmd_reg.write().unwrap();
+    registry.register_new_process(command_name.to_owned(), process);
+
+    Ok(())
 }
 
 #[post("/run/<command_name>")]
-fn execute(command_name: &str) -> Json<Result<u32, String>> {
-    Json(execute_inner(command_name))
+fn execute(
+    command_name: &str,
+    config: &rocket::State<Config>,
+    cmd_reg: &rocket::State<RwLock<command::Registry>>,
+) -> Json<Result<(), String>> {
+    Json(execute_inner(command_name, config, cmd_reg))
 }
 
 #[launch]
@@ -100,7 +93,9 @@ fn rocket() -> _ {
 
     let config = configuration::parse(&config_contents)
         .expect("configuration file contents should be valid");
-    *CONFIG.write().unwrap() = config;
 
-    rocket::build().mount("/", routes![list_commands, get_status, execute])
+    rocket::build()
+        .manage(RwLock::new(Registry::new(&config)))
+        .manage(config)
+        .mount("/", routes![list_commands, get_status, execute])
 }
